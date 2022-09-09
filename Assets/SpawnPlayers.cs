c4600cf437243bee55c67b82343e840c825e6f73
shercor <sebastian.herrerac@sansano.usm.cl>
2022-09-09 08:26:32 -0400

    Cazador está sincronizado en sesión multiplayer, falta conectar con lobby final

--- Assets/SpawnPlayers.cs
+++ Assets/SpawnPlayers.cs
@@ -7,7 +7,7 @@ public class SpawnPlayers : MonoBehaviour
 {
     public GameObject playerPrefab;
     public GameObject giantApple;
-    //public string hunterPrefab;
+    public GameObject hunterPrefab;
     
     public float minX;
     public float minY;
@@ -21,11 +21,19 @@ public class SpawnPlayers : MonoBehaviour
         Vector3 randomPosition = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), Random.Range(minZ, maxZ));
         GameObject apple = PhotonNetwork.Instantiate(giantApple.name, new Vector3(Random.Range(-115, -110), 116, 513), Quaternion.identity);
         GameObject player = PhotonNetwork.Instantiate(playerPrefab.name, playerPrefab.transform.position, Quaternion.identity);
-        //GameObject enemy = PhotonNetwork.Instantiate(, new Vector3(Random.Range(-115, -110), 116, 513), Quaternion.identity);
+        StartCoroutine(HunterSpawn());
+        
         if(1 == 1){
             Debug.Log(":O");
         }
     }
+
+    IEnumerator HunterSpawn(){
+        yield return new WaitForSeconds(3);
+        GameObject enemy = PhotonNetwork.Instantiate(hunterPrefab.name, new Vector3(Random.Range(-115, -110), 116, 513), Quaternion.identity);
+    }
+        
+    
 
 
 }
