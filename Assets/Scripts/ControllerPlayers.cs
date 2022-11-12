d0d499540c3793b84f58281f24d608209698f31a
shercor <sebastian.herrerac@sansano.usm.cl>
2022-11-12 19:04:47 -0400

    Cambios en Controller Player en Guanaco para desactivar componentes

--- Assets/Scripts/ControllerPlayers.cs
+++ Assets/Scripts/ControllerPlayers.cs
@@ -19,6 +19,32 @@ public class ControllerPlayers : MonoBehaviour
 
     void Start()
     {
+        this.GetComponent<CharacterController>().enabled = false;
+        //this.GetComponent<XROrigin>().enabled = false;
+        //this.GetComponent<ContinuousMovement>().enabled = false;
+        //this.GetComponent<LocomotionSystem>().enabled = false;
+        //this.GetComponent<LocomotionController>().enabled = false;
+        //this.GetComponent<TeleportationProvider>().enabled = false;
+        MonoBehaviour[] comps = GetComponents<MonoBehaviour>();
+        
+        foreach(MonoBehaviour c in comps)
+        {
+            if(c.GetType().Name == "XROrigin"){
+                c.enabled = false;
+            }
+            if(c.GetType().Name == "ContinuousMovement"){
+                c.enabled = false;
+            }
+            if(c.GetType().Name == "LocomotionSystem"){
+                c.enabled = false;
+            }
+            if(c.GetType().Name == "LocomotionController"){
+                c.enabled = false;
+            }
+            if(c.GetType().Name == "TeleportationProvider"){
+                c.enabled = false;
+            }
+        }
         view = GetComponent<PhotonView>();
         for (int i = 0; i < gameObject.transform.childCount; i++)
         {
@@ -30,6 +56,25 @@ public class ControllerPlayers : MonoBehaviour
 
         if (view.IsMine)
         {
+            this.GetComponent<CharacterController>().enabled = true;
+            foreach(MonoBehaviour c in comps)
+            {
+                if(c.GetType().Name == "XROrigin"){
+                    c.enabled = true;
+                }
+                if(c.GetType().Name == "ContinuousMovement"){
+                    c.enabled = true;
+                }
+                if(c.GetType().Name == "LocomotionSystem"){
+                    c.enabled = true;
+                }
+                if(c.GetType().Name == "LocomotionController"){
+                    c.enabled = true;
+                }
+                if(c.GetType().Name == "TeleportationProvider"){
+                    c.enabled = true;
+                }
+            }
             if (gameObject.GetPhotonView().IsMine){
                 for (int i = 0; i < gameObject.transform.childCount; i++)
                 {
