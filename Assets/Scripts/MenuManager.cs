bd72167c33ed2557a661683caa0dc40deadda953
MarcosMaldonadoMerino <79269941+MarcosMaldonadoMerino@users.noreply.github.com>
2022-11-16 14:49:10 -0300

    Lobby VR Juegos

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,48 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] Menu[] menus;
+    public TMP_Text textoNombreSala;
     public static MenuManager Instance;
 
-    void Awake(){
+    void Awake() {
         Instance = this;
     }
 
     public void OpenMenu(string menuName)
     {
-        for(int i = 0; i < menus.Length; i++)
+        for (int i = 0; i < menus.Length; i++)
         {
-            if(menus[i].menuName == menuName)
+            if (menus[i].menuName == menuName)
             {
                 //OpenMenu(menus[i]);
                 menus[i].Open();
             }
-            else if(menus[i].open)
+            else if (menus[i].open)
             {
-                CloseMenu(menus[i]);    
+                CloseMenu(menus[i]);
             }
         }
     }
 
     public void OpenMenu(Menu menu)
     {
-        for(int i = 0; i < menus.Length; i++)
+        for (int i = 0; i < menus.Length; i++)
         {
-            if(menus[i].open)
+            if (menus[i].open)
             {
-                CloseMenu(menus[i]);    
+                CloseMenu(menus[i]);
             }
+        }
+        if (menu.menuName == "room")
+        {
+            string nombreSala = gameObject.GetComponent<RoomLauncher>().nombreSala;
+            textoNombreSala.text = nombreSala;
         }
         menu.Open();
     }
 
     public void CloseMenu(Menu menu) {
-        
+
         menu.Close();
-        
+
+    }
+    public void SalirAlMuseo()
+    {
+        SceneManager.LoadScene("Museo VR");
     }
 }
